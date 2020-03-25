9cc4179324162884683610da704e0e08ae3a995e
Emre Cilekci <emre.cilekci@trendyol.work>
2020-03-25 13:49:50 +0300

    hesap makinesi eklendi

---
+++ calculator.py
@@ -0,0 +1,18 @@
+# Create By Emre Çilekci//
+islem = """
+(1) Topla
+(2) Çıkar
+"""
+print(islem)
+soru=input("Lütfen İşlem Seçiniz")
+girdi = "İLK SAYI:  {} , İKİNCİ SAYI:  {}"
+if soru not in  islem:
+        print("Yanlış Seçim Yaptınız")
+elif soru == "1":
+    sayı1 = int(input("Toplama işlemi için ilk sayıyı girin:   "))
+    sayı2 = int(input("Toplama işlemi için ikinci sayıyı girin:  "))
+    print(girdi.format(sayı1, sayı2), "için toplama işleminin sonucu", (sayı1 + sayı2))
+elif soru == "2":
+    sayı1 = int(input("Çıkarma işlemi için ilk sayıyı girin:   "))
+    sayı2 = int(input("Çıkarma işlemi için ikinci sayıyı girin:  "))
+    print(girdi.format(sayı1,sayı2),"için çıkarma işleminin sonucu",(sayı1-sayı2))
