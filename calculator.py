6cad06a26f532e0e9b1fccf500a45a4b9fc30cc9
Emre Cilekci <emre.cilekci@trendyol.work>
2020-03-25 13:50:35 +0300

    boşluklar silindi

--- calculator.py
+++ calculator.py
@@ -6,13 +6,18 @@ islem = """
 print(islem)
 soru=input("Lütfen İşlem Seçiniz")
 girdi = "İLK SAYI:  {} , İKİNCİ SAYI:  {}"
+
 if soru not in  islem:
         print("Yanlış Seçim Yaptınız")
+
 elif soru == "1":
     sayı1 = int(input("Toplama işlemi için ilk sayıyı girin:   "))
     sayı2 = int(input("Toplama işlemi için ikinci sayıyı girin:  "))
+
     print(girdi.format(sayı1, sayı2), "için toplama işleminin sonucu", (sayı1 + sayı2))
+
 elif soru == "2":
     sayı1 = int(input("Çıkarma işlemi için ilk sayıyı girin:   "))
     sayı2 = int(input("Çıkarma işlemi için ikinci sayıyı girin:  "))
+
     print(girdi.format(sayı1,sayı2),"için çıkarma işleminin sonucu",(sayı1-sayı2))
